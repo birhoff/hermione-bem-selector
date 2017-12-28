d78b6aadc0729ca73775a3036bf6f73d2567f5d3
Konstantin Gladkikh <birhoff@users.noreply.github.com>
2017-12-28 16:21:52 +0200

    Исправление под новую версию гермионы

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ module.exports = function(hermione, opts) {
 
     const options = Object.assign({}, _defaults, opts);
 
-    hermione.on(hermione.events.RUNNER_START, function() {
+    hermione.on(hermione.events.BEFORE_FILE_READ, function() {
         global[options.selector] = selector;
     });
 };
